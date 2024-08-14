40f0399d1688d90e6dffb221ef56046cb8f99d53
Ordep-42 <90785456+Ordep-42@users.noreply.github.com>
2024-08-14 19:26:21 -0300

    fix: wrogn MQTT feed key

--- src/broker.py
+++ src/broker.py
@@ -33,5 +33,5 @@ def broker():
     while True:
         with lock:
             if shared_data["total"] is not None:
-                client.publish("Ordep_1/feed/room-people-entradas", shared_data["total"])
+                client.publish("Ordep_1/feeds/room-people-detector.entrada", shared_data["total"])
         sleep(15)
